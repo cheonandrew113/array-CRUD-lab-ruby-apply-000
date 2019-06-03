c94d4bcab07411e7ff9ee538b99ec962c1e76534
Andnrew Cheon <cheonandrew113@gmail.com>
2019-06-03 03:38:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -21,7 +21,7 @@ end
 
 def remove_element_from_start_of_array(array)
   ["I", "am", "really", "learning", "arrays!"]
-  array.shift[0]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
