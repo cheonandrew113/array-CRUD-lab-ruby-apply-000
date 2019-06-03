3c7747f5112884a3c03d375a2edf5bde80a88d52
Andnrew Cheon <cheonandrew113@gmail.com>
2019-06-03 03:40:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,13 +25,16 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  ["wow", "I", "am", "really", "learning", "arrays!"]
+  array.index_number[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  ["wow", "I", "am", "really", "learning", "arrays!"]
+  array[5]
 end
